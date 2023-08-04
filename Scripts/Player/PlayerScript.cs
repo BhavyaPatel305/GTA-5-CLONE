c61a5ef5bca0b25efea8f603fb068e1393e57fc7
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-08-05 04:00:20 +0530

    Making Player Rotation Smooth(Link) Link: https://www.youtube.com/watch?v=1ILZSDHDv0s

--- Scripts/Player/PlayerScript.cs
+++ Scripts/Player/PlayerScript.cs
@@ -14,6 +14,11 @@ public class PlayerScript : MonoBehaviour
     // Attach player's character controller
     public CharacterController cC;
 
+    [Header("Player Jumping & Velocity")]
+    // To make the player rotation smooth
+    public float smoothTurnTime = 0.1f;
+    private float smoothTurnVelocity;
+
     // Update Function
     private void Update(){
         // Call the playerMove function
@@ -40,9 +45,12 @@ public class PlayerScript : MonoBehaviour
             // Rad2Deg will convert radians to degrees
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
 
+            // Get the new angle value using SmoothDampAngle() method
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurnVelocity, smoothTurnTime);
+
             // Rotate the player using targetAngle
             // What this Quaternion.Euler() does is that it returns a rotation that rotates z degrees around z-axis
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             // Now using character controller, we will move the player
             cC.Move(direction.normalized * playerSpeed * Time.deltaTime);
