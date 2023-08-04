24d8dfc169d47e6794b1f54db50591db5007b934
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-08-05 03:47:56 +0530

    Adding Player Rotation(Link) Link: https://www.youtube.com/watch?v=rKzGLp7nbNg

--- Scripts/Player/PlayerScript.cs
+++ Scripts/Player/PlayerScript.cs
@@ -35,6 +35,15 @@ public class PlayerScript : MonoBehaviour
 
         // direction.magnitude >= 0.1f meaning player is moving
         if(direction.magnitude >= 0.1f){
+            // Adding rotation to the player
+            // Atan2() function will convert angle into radians
+            // Rad2Deg will convert radians to degrees
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            // Rotate the player using targetAngle
+            // What this Quaternion.Euler() does is that it returns a rotation that rotates z degrees around z-axis
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
             // Now using character controller, we will move the player
             cC.Move(direction.normalized * playerSpeed * Time.deltaTime);
         }
