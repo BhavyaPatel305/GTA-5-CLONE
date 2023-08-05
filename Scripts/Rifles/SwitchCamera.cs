eee659e7a6c0a6147fc36e27b2126cfb99f93096
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-08-05 23:14:37 +0530

    CrossHair Functionality: Switching Camera between aim camera and 3rd person camera Link: https://www.youtube.com/watch?v=sw-RgDVnN5A

--- Scripts/Rifles/SwitchCamera.cs
+++ Scripts/Rifles/SwitchCamera.cs
@@ -5,5 +5,37 @@ using UnityEngine;
 public class SwitchCamera : MonoBehaviour
 {
     // Adding CrossHair/Aiming functionality
-    
+    // We need this SwitchCamera script as without it if we run the game
+    // we can just see cross hair in the middle of the screen and if we turn the player
+    // in some other direction than the direction of the gun pointing and the cross hair would be different
+
+    // We will be switching from 3rd person camera to aim camera, so for that we need different header names
+    [Header("Camera to Assign")]
+    // So we will have 2 references
+    public GameObject AimCam;
+    public GameObject ThirdPersonCam;
+
+    private void Update(){
+        // If we press mouse right button and if we are walking
+        // Meaning if we are aiming and moving at the same time than what we want to do
+        if(Input.GetButton("Fire2") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
+            // We want the 3rd person camera to be disabled
+            ThirdPersonCam.SetActive(false);
+            // Activate the aim cam
+            AimCam.SetActive(true);
+        }
+        // If player is just pressing mouse right button
+        else if(Input.GetButton("Fire2")){
+            // We want the 3rd person camera to be disabled
+            ThirdPersonCam.SetActive(false);
+            // Activate the aim cam
+            AimCam.SetActive(true);
+        }else{
+            // If the player is not aiming, activate the 3rd person camera and aim cam to be deactivated
+            ThirdPersonCam.SetActive(true);
+            AimCam.SetActive(false);
+        }
+    }
+
+
 }
