f997dcc94790de079f21da318806959654e4738f
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-08-09 07:44:54 +0530

    Adding money to the game for buying weapons from weapon store Link: https://www.youtube.com/watch?v=boM2PTEGpCM

---
+++ Scripts/Player/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Adding money to the game for buying weapons from weapon store
+    [Header("Player Money")]
+    // PLayer bank balance
+    public int playerMoney;
+}
