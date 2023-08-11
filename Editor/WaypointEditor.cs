dae10f3f9edaeec77a3f37bb6e0c7587fd8b0409
Bhavya Patel <93842768+BhavyaPatel305@users.noreply.github.com>
2023-08-12 00:20:59 +0530

    Next & Previous Waypoint Connections Link: https://www.youtube.com/watch?v=bBm5JdjU1Ew&t=23s

--- Editor/WaypointEditor.cs
+++ Editor/WaypointEditor.cs
@@ -24,5 +24,37 @@ public class WaypointEditor : Editor
         // Draw a white line for way point width
         Gizmos.color = Color.white;
         Gizmos.DrawLine(waypoint.transform.position + (waypoint.transform.right * waypoint.waypointWidth/2f), (waypoint.transform.position - (waypoint.transform.right * waypoint.waypointWidth/2f)));
+
+        // Connect waypoints
+        // Drawing line from previous to next way point and from next way point to previous waypoint
+        if(waypoint.previousWaypoint != null){
+            // If previous way point is not null then
+
+            // This will color line in red color
+            Gizmos.color = Color.red;
+            // Here we take current way point position and then 
+            Vector3 offset = waypoint.transform.right * waypoint.waypointWidth/2f;
+            // Here we take previous way point position
+            Vector3 offsetTo = waypoint.previousWaypoint.transform.right * waypoint.previousWaypoint.waypointWidth/2f;
+            // From current way point to previous way point, we created a line(of red color)
+            Gizmos.DrawLine(waypoint.transform.position + offset,waypoint.previousWaypoint.transform.position + offsetTo);
+        }
+
+        // Connect waypoints
+        // Drawing line from previous to next way point and from next way point to previous waypoint
+        if(waypoint.nextWaypoint != null){
+            // If next way point is not null then
+
+            // This will color line in green color
+            Gizmos.color = Color.green;
+            // Drawing line from current waypoint to next way point
+
+            // Here we take current way point position and then, (NOTICE: Here we are using -ve sign as we cant use .left key word)
+            Vector3 offset = waypoint.transform.right * -waypoint.waypointWidth/2f;
+            // Here we take previous way point position(NOTICE: Here we are using -ve sign as we cant use .left key word)
+            Vector3 offsetTo = waypoint.nextWaypoint.transform.right * -waypoint.nextWaypoint.waypointWidth/2f;
+            // From current way point to previous way point, we created a line(of red color)
+            Gizmos.DrawLine(waypoint.transform.position + offset, waypoint.nextWaypoint.transform.position + offsetTo);
+        }
     }
 }
